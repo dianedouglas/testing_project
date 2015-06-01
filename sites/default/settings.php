eb8e425664f77b1db4dbdd4a92199edadb0ee579
Diane Douglas <diane.douglas1@gmail.com>
2015-05-31 23:49:19 -0700

    Installation.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'testing_project',
+      'username' => 'testing_project',
+      'password' => 'testing_project',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'NPIWKzeGjPaO08Po8xM4x8X3Xv6ngQDZUtMwMESAZfs';
 
 /**
  * Base URL (optional).
